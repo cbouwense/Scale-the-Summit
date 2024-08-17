91a4f1e8208e8063166ad1d6b5d627ab017b9af5
Christian Bouwense <cbouwense@proton.me>
2024-08-17 17:55:20 -0400

    obstacles

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,28 @@
 class_name Player extends Node2D
 
+@onready var ray_cast_2d_up: RayCast2D = $Area2D/CollisionShape2D/RayCast2DUp
+@onready var ray_cast_2d_down: RayCast2D = $Area2D/CollisionShape2D/RayCast2DDown
+@onready var ray_cast_2d_left: RayCast2D = $Area2D/CollisionShape2D/RayCast2DLeft
+@onready var ray_cast_2d_right: RayCast2D = $Area2D/CollisionShape2D/RayCast2DRight
+
+
 func _ready():
 	position = g.player_starting_position
 
 func do_something(action: g.CardAction):
 	match action:
 		g.CardAction.UP:
-			position.y -= 16
+			if !ray_cast_2d_up.is_colliding():
+				position.y -= 16
+		
 		g.CardAction.DOWN:
-			if position.y < -1:
+			if position.y < -1 and !ray_cast_2d_down.is_colliding():
 				position.y += 16
 		g.CardAction.LEFT:
-			if position.x >= -8:
+			if position.x >= -8 and !ray_cast_2d_left.is_colliding():
 				position.x -= 16
 		g.CardAction.RIGHT:
-			if position.x <= 24:
+			if position.x <= 24 and !ray_cast_2d_right.is_colliding():
 				position.x += 16
 
 	# Did we reach the goal
